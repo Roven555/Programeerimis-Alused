7c450f74a10ee348ad200001d3365536987beb33
Student <student@Administrators-Mac-mini.local>
2025-10-15 15:15:22 +0300

    sui

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log(`Hello World`);
+console.log(`I like pizza`);
+
+window.alert(`This is an alert!`)
+window.alert(`I like pizza`)
+
+document.getElementById(myH1).textContent = `Hello`
+document.getElementById(myP).textcontent = `I like pizza`
+
+// This is a comment
+
+/*
+this
+is
+a
+comment
+*/
+
+
+
+let x;
+x = 100;
+
+console.log(x)
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1
+
+console.log(`You are ${age} years old`);
+console.log(`the price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+
+
+ 
