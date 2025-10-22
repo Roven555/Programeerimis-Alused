882cb81e94febf11c2870fe751f82f95b1a89a2a
Roven Piho <roven.piho@voco.ee>
2025-10-22 22:53:53 +0300

    sseew

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// Ask the user for two integers
+/*
 let a = prompt("a = ?");
 let b = prompt("b = ?");
 
-// Convert the input strings to integers
 a = parseInt(a);
 b = parseInt(b);
 
-// Calculate the sum
 let sum = a + b;
 
-// Display the result on the page
-document.getElementById("result").textContent = "The sum is: " + sum;
+document.getElementById("result").textContent = "sum on: " + sum;
+*/
+
+var a = 10;
+var b = 20;
+
+var temp = a;
+a = b;
+b = temp;
+
+document.write("a = " + a + ", b = " + b);
