ce081df333c732f3c486a2e4c7654e7445e80101
Student <student@Administrators-Mac-mini.local>
2025-10-23 11:24:26 +0300

    switches

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let testScore= 60;
+let testScore= 90;
 let letterGrade;
 
 switch(true){
     case testScore >= 90:
         letterGrade = "A";
         break;
-        ase testScore >= 80:
+        case testScore >= 80:
         letterGrade = "B";
         break;
-        ase testScore >= 70:
+        case testScore >= 70:
         letterGrade = "C";
         break;
-        ase testScore >= 60:
+        case testScore >= 60:
         letterGrade = "D";
         break;
         default:
