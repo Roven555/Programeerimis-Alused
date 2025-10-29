8c513adb49b46e260e3766498abf977ce7043996
Student <student@Administrators-Mac-mini.local>
2025-10-29 15:25:18 +0200

    loop

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-function kontrolliVanust() {
-  let sisend = prompt("Sisesta oma vanus:");
+// while loop = repeat some code while some condition is true
 
-  if (sisend === "" || isNaN(sisend)) {
-    alert("Sisend ei ole korrektne. Sisesta oma vanus.");
-    kontrolliVanust(); return;
-    
-  }
+/*
 
-  let vanus = Number(sisend);
+let username = "";
 
-  if (vanus >= 7 && vanus <= 16) {
-    alert("Sööd kell 10.");
-  } else if (vanus >= 17 && vanus <= 20) {
-    alert("Sööd kell 11.");
-  } else if (vanus >= 20 && vanus <= 125 ) {
-    alert("Sööd kell 12.");
-  } else {
-    alert("Sisend ei ole korrektne. Sisenda korrektne vanus (7-125a).");
-  }
+while(username === "" || username === null ){
+  username = window.prompt(`Enter your name`)
 }
 
-kontrolliVanust();
+console.log(`Hello ${username}`);
+*/
+
+/* let loggedIn = false;
+let username;
+let password;
+
+do{
+  username = window.prompt(`Enter your username`);
+  password = window.prompt(`Enter your password`);
+
+  if (username === "myUsername" && password === "myPassword") {
+    loggedIn = true;
+    console.log("You are logged in!"); 
+  } else {
+    console.log("Pass vale! proovi uuesti");
+  }
+}while(!loggedIn)
+*/ 
+
+// for loop = repeat some code a limited amount of times
+
+/*
+for(let i = 10; i > 0; i-=2){
+console.log(i)
+}
+
+console.log("Happy new year!")
+*/
+
+/* Continue jatab 13 vahele
+for(let i = 1; i <= 20; i++){
+ if(i ==13){
+  continue;
+ }
+ else{
+  console.log(i);
+ }
+}
+*/
+
+/* Kuni 13ni
+for(let i = 1; i <= 20; i++){
+ if(i ==13){
+  break;
+ }
+ else{
+  console.log(i);
+ }
+}
+ */
+
+
+
+
