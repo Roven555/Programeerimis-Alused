a1e070b836bd337ab27ac5616d5938d6c02bee33
Student <student@Administrators-Mac-mini.local>
2025-10-29 16:28:54 +0200

    numbr

--- index.js
+++ index.js
@@ -1,65 +1,25 @@
-// while loop = repeat some code while some condition is true
+// number guess
 
-/*
 
-let username = "";
+const minNum = 80;
+const maxNum = 90;
+const answer = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
 
-while(username === "" || username === null ){
-  username = window.prompt(`Enter your name`)
+let vastus = window.prompt("Arva number vahemikus 80-90")
+
+
+if(vastus = answer) {
+ alert("Arvasid ära");
+} else if (vastus < answer) {
+alert("Number on natuke suurem!");
+} else if (vastus > answer) {
+ alert("Number on natuke väike!");
+} else {
+alert("Sisesta arv!");
 }
 
-console.log(`Hello ${username}`);
-*/
-
-/* let loggedIn = false;
-let username;
-let password;
-
-do{
-  username = window.prompt(`Enter your username`);
-  password = window.prompt(`Enter your password`);
-
-  if (username === "myUsername" && password === "myPassword") {
-    loggedIn = true;
-    console.log("You are logged in!"); 
-  } else {
-    console.log("Pass vale! proovi uuesti");
-  }
-}while(!loggedIn)
-*/ 
-
-// for loop = repeat some code a limited amount of times
-
-/*
-for(let i = 10; i > 0; i-=2){
-console.log(i)
-}
-
-console.log("Happy new year!")
-*/
-
-/* Continue jatab 13 vahele
-for(let i = 1; i <= 20; i++){
- if(i ==13){
-  continue;
- }
- else{
-  console.log(i);
- }
-}
-*/
-
-/* Kuni 13ni
-for(let i = 1; i <= 20; i++){
- if(i ==13){
-  break;
- }
- else{
-  console.log(i);
- }
-}
- */
-
+console.log(answer)
+ 
 
 
 
